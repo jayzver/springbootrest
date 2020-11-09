65c1a62638fdeb6f555cba04f8b902147d3c5fd7
jayzver <phonem5note@gmail.com>
2020-11-09 14:39:06 +0200

    first start widh error in database

---
+++ src/main/java/net/oneqas/OneqasMain.java
@@ -0,0 +1,13 @@
+package net.oneqas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneqasMain
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OneqasMain.class);
+    }
+}
